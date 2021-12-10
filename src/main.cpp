3e138bc8bacf97e97e7cfa536b769cae56e2ee6b
Everymen <gajada89@gmail.com>
2021-12-10 15:19:52 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file main.cpp
+ * @author Adam Gajda (xgajda07@stud.fit.vutbr.cz), Jan Doležel (xdolez81@stud.fit.vutbr.cz)
+ * @brief Simulation of bakery and it's supply chain
+ * 
+ * @date 2021-12-12
+ */
+
+#include "simlib.h"
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
