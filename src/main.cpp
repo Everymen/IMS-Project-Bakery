202fdb37b77be66b5c17bc07a6f7a6298af1ce81
Everymen <gajada89@gmail.com>
2021-12-10 17:31:13 +0100

    Add macros

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,41 @@
 /**
  * @file main.cpp
  * @author Adam Gajda (xgajda07@stud.fit.vutbr.cz), Jan Doležel (xdolez81@stud.fit.vutbr.cz)
- * @brief Simulation of bakery and it's supply chain
+ * @brief Simulation of bakery and it's supply chain with SIMLIB
  * 
  * @date 2021-12-12
  */
 
+/**
+ * @brief LIBRARIES
+ * 
+ */
 #include "simlib.h"
 
+/**
+ * @brief MACROS
+ * 
+ */
+#define WHEAT_GENERATOR 10
+#define WHEAT_THRESHER 22
+#define WHEAT_MILL 1
+#define BAKER 5
+#define INGREDIENT_MIXER 1
+#define DOUGH_MIXER 1
+#define DOUGH_DIVIDER 1
+#define ROUNDING_TABLE 2
+#define OVEN 2
+#define SHOPKEEPER 1
+
+
+
+/**
+ * @brief Main body of simulation
+ * 
+ * @param argc num of arguments
+ * @param argv argument values
+ * @return int return code
+ */
 int main(int argc, char *argv[])
 {
     return 0;
